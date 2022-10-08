36adde6ed487c531b64d817dc6845776adcd03b6
Hanan88 <Hanan.Ahmed.8@hotmail.com>
2022-10-08 22:26:16 +0200

    update: footer responsive

--- src/components/layouts/Footer/index.js
+++ src/components/layouts/Footer/index.js
@@ -7,55 +7,60 @@ import { MdLocationPin } from 'react-icons/md';
 import './footer.css'
 const Footer = () => {
   return (
-    <div className='container px-20 py-20 bg-main-blue flex justify-between '>
-      <img src={logo} alt='' className='w-48 h-fit pt-16' />
+    <div className='bg-main-blue '>
+      <div className='container px-20 py-20 flex justify-between 
+      lg:flex-row md:flex-col sm:flex-col'>
+        <img src={logo} alt='' className='w-48 h-fit pt-16' />
 
-      <div className='details text-white flex w-1/2 justify-around '>
-        <div><h2>COMPANY</h2>
-          <ul>
-            <li>About The Company</li>
-            <li>Continental Bed</li>
-            <li>Certificates</li>
-            <li>References</li>
-            <li>Contact Information</li>
-          </ul>
-        </div>
+        <div className='details text-white flex w-1/2 
+        lg:justify-around md:justify-start sm:justify-start
+        lg:w-1/2 md:w-full sm:w-full md:mt-8 md:pt-18'>
+          <div><h2>COMPANY</h2>
+            <ul>
+              <li>About The Company</li>
+              <li>Continental Bed</li>
+              <li>Certificates</li>
+              <li>References</li>
+              <li>Contact Information</li>
+            </ul>
+          </div>
 
-        <div><h2>Mattress</h2>
-          <ul>
-            <li>Bliss Line </li>
-            <li>Sleep Spa </li>
-            <li>Essential</li>
-          </ul>
-        </div>
+          <div><h2>Mattress</h2>
+            <ul>
+              <li>Bliss Line </li>
+              <li>Sleep Spa </li>
+              <li>Essential</li>
+            </ul>
+          </div>
 
-        <div className='w-1/4'> <h2>Showrooms</h2>
-          <ul>
-            <li className='flex'> <MdLocationPin className='-mt-4 text-6xl' />90 Avenue South Second
-              Sector Bldg 317, First Floor
-              Fifth Settlement.</li>
-            <li className='flex'><MdLocationPin className='-mt-4 text-6xl' /> Zayed Dunes Commercial
-              Bldg 13, Unit 3  Sixth District
-              Sheikh Zayed.</li>
-          </ul>
-        </div>
-        <div><h2>FIND US</h2>
-          <div className='flex flex-col items-center text-3xl text-slate-400 gap-3'>
-            <FaFacebookF />
-            <AiOutlineTwitter />
-            <AiFillInstagram />
+          <div> <h2>Showrooms</h2>
+            <ul>
+              <li className='flex'> <MdLocationPin className='-mt-4 text-6xl' />90 Avenue South Second
+                Sector Bldg 317, First Floor
+                Fifth Settlement.</li>
+              <li className='flex'><MdLocationPin className='-mt-4 text-6xl' /> Zayed Dunes Commercial
+                Bldg 13, Unit 3  Sixth District
+                Sheikh Zayed.</li>
+            </ul>
+          </div>
+          <div><h2>FIND US</h2>
+            <div className='flex flex-col items-center text-3xl text-slate-400 gap-3'>
+              <FaFacebookF />
+              <AiOutlineTwitter />
+              <AiFillInstagram />
+            </div>
           </div>
         </div>
+
+
+
+        <div className='Newsletter lg:w-1/4 border rounded p-5 md:1/2 sm:1/2'>
+          <h2 className='text-white text-2xl'>Newsletter</h2>
+          <p className='text-slate-300 leading-5 my-3'>Subscribe Our Newletter to receive updates on new arrivals, special offers and other discount information.</p>
+          <input type='text' className='mt-5 p-2 bg-slate-400' placeholder='Email' /><button className='bg-white py-2 px-5 rounded-r'>Send</button>
+        </div>
+
       </div>
-
-
-
-      <div className='Newsletter w-1/4 border rounded  p-5'>
-        <h2 className='text-white text-2xl'>Newsletter</h2>
-        <p className='text-slate-300 leading-5 my-3'>Subscribe Our Newletter to receive updates on new arrivals, special offers and other discount information.</p>
-        <input type='text' className='mt-5 p-2 bg-slate-400' placeholder='Email' /><button className='bg-white py-2 px-5 rounded-r'>Send</button>
-      </div>
-
     </div>
   )
 }
